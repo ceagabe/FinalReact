927d6276021b7dcc04fd61eef4d6fc3014fab5e2
ceagabe <gabriel_benedetti@hotmail.com>
2025-11-30 21:42:27 -0300

    septimo commit

--- src/pages/Servicios.jsx
+++ src/pages/Servicios.jsx
@@ -4,14 +4,16 @@ import { Link } from 'react-router-dom'
 function Servicios() {
   return (
     <>
-    <h1>Servicios</h1>
+    <div style={{textAlign: 'center'}}>
+        <h1>Servicios</h1>
             <hr />
     <div style={{ width: '85%', margin: '0 auto' }}>
-        <h1> Servicios </h1>
-        <h3>servicio mecánico, laminado y pintura, refacciones, accesorios*, servicios financieros y seguros a clientes particulares, empresas e instituciones, así como desarrollos inmobiliarios*. Trabajamos con marcas que ofrecen productos y servicios de reconocida calidad, confiabilidad y seguridad. El Cliente es la prioridad de nuestra organización y le brindamos asesoría integral de nuestros productos y servicios, así como herramientas financieras que se adaptan a las necesidades particulares.</h3>
+        
+        <p>Servicio mecánico, laminado y pintura, refacciones, accesorios, servicios financieros y seguros a clientes particulares, empresas e instituciones, así como desarrollos inmobiliarios. Trabajamos con marcas que ofrecen productos y servicios de reconocida calidad, confiabilidad y seguridad. El Cliente es la prioridad de nuestra organización y le brindamos asesoría integral de nuestros productos y servicios, así como herramientas financieras que se adaptan a las necesidades particulares.</p>
         <hr />
-        <li><Link to= "/"> <button>Inicio </button>  </Link></li>
+        <Link to= "/"> <button>Inicio </button>  </Link>
          </div>
+    </div>
     </>
   )
 }
