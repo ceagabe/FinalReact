0ee99e67ac2c9017d32097c480507aebadf4ea55
ceagabe <gabriel_benedetti@hotmail.com>
2025-11-30 21:56:12 -0300

    septimo commit

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function Footer() {
   return (
+    
     <footer>
         <hr />
         <p>Desarrolado por: @mati.campos y modificado por @ Gabriel Benedetti </p>
+    
     </footer>
   )
 }
